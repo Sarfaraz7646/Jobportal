ea27a4faffea3afaec610fbc392c9baee6b2a9c4
Sarfaraz7646 <sarfarazalex7646@gmail.com>
2025-07-15 13:48:18 +0530

    yes

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+const whitelist = [process.env.FRONTEND_URL];
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || whitelist.includes(origin)) {
